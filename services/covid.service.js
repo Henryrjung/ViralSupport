c47919023ef0183b427a065f506be015c0c02f49
Henry <henryjung667@gmail.com>
2020-12-19 14:20:50 -0500

    small fixes

--- services/covid.service.js
+++ services/covid.service.js
@@ -1,17 +1,17 @@
-var axios = require("axios").default;
+const axios = require('axios').default;
 
 const CovidService = {
-  getStats: function (country) {
+  getStats: function(country) {
     var options = {
-        method: 'GET',
-        url: 'https://covid-193.p.rapidapi.com/statistics',
-        params: {country: country,},
-        headers: {
-          'x-rapidapi-key': 'ba8be4669fmshee0e414c4ce2a90p1a3daajsn012f6ec8285e',
-          'x-rapidapi-host': 'covid-193.p.rapidapi.com'
-        }
-      };
-      
+      method: 'GET',
+      url: 'https://covid-193.p.rapidapi.com/statistics',
+      params: { country: country },
+      headers: {
+        'x-rapidapi-key': 'ba8be4669fmshee0e414c4ce2a90p1a3daajsn012f6ec8285e',
+        'x-rapidapi-host': 'covid-193.p.rapidapi.com',
+      },
+    };
+
     // console.log(options)
     return axios.request(options);
   },
