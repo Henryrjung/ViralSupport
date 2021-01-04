7001c6f694c108a86b8d8d9364e1d46a804d7e0c
Victoria Rouser <vic86rous@gmail.com>
2021-01-04 03:52:58 -0600

    cleaned up news.js

--- services/news.service.js
+++ services/news.service.js
@@ -1,30 +1,52 @@
-const http = require("https");
+const axios = require("axios").default;
 const Parser = require ("rss-parser")
 const parser = new Parser()
 
-const options = {
-	"method": "GET",
-	"hostname": "coronavirus-smartable.p.rapidapi.com",
-	"port": null,
-	"path": "/news/v1/US/",
-	"headers": {
-		"x-rapidapi-key": "22539d0047mshaba23ae76c1a0c3p1d7388jsn5281cea1a33d",
-		"x-rapidapi-host": "coronavirus-smartable.p.rapidapi.com",
-		"useQueryString": true
-	}
+const Covid19News = {
+	getNews: (country) => {
+		const options = {
+			method: 'GET',
+			url: 'https://coronavirus-smartable.p.rapidapi.com/news/v1/US/',
+			params: {country: country },
+			headers: {
+			  'x-rapidapi-key': '22539d0047mshaba23ae76c1a0c3p1d7388jsn5281cea1a33d',
+			  'x-rapidapi-host': 'coronavirus-smartable.p.rapidapi.com',
+			},
+		  };
+
+		return axios.request(options);
+	},
+
+	getLocalNews: (country) => {
+		const options = {
+			method: 'GET',
+			url: 'https://coronavirus-smartable.p.rapidapi.com/news/v1/US/',
+			params: {country: country },
+			headers: {
+			  'x-rapidapi-key': '22539d0047mshaba23ae76c1a0c3p1d7388jsn5281cea1a33d',
+			  'x-rapidapi-host': 'coronavirus-smartable.p.rapidapi.com',
+			},
+		  };
+
+
+		return axios.request(options);
+	},
+
+	getNationalNews: (country) => {
+		const options = {
+			method: 'GET',
+			url: 'https://coronavirus-smartable.p.rapidapi.com/news/v1/US/',
+			params: {country: country },
+			headers: {
+			  'x-rapidapi-key': '22539d0047mshaba23ae76c1a0c3p1d7388jsn5281cea1a33d',
+			  'x-rapidapi-host': 'coronavirus-smartable.p.rapidapi.com',
+			},
+		  };
+
+
+		return axios.request(options);
+	},
 };
 
-const req = http.request(options, function (res) {
-	const chunks = [];
 
-	res.on("data", function (chunk) {
-		chunks.push(chunk);
-	});
-
-	res.on("end", function () {
-		const body = Buffer.concat(chunks);
-		console.log(body.toString());
-	});
-});
-
-req.end();
+module.exports = Covid19News;
