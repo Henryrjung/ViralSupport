33e21f1011c549dabfc3220c1132eadc740e667e
HenryJ <henryjung667@gmail.com>
2021-01-05 22:01:34 -0600

    created blog posts

--- database/schema.sql
+++ database/schema.sql
@@ -4,24 +4,24 @@ CREATE DATABASE viral_support_db;
 
 USE viral_support_db;
 
-CREATE TABLE stats (
-    covid19 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    number_of_cases DECIMAL, 
-    number_of_death DECIMAL,
-    number_of_recoveries DECIMAL,
-    PRIMARY KEY (covid19)
-);
+-- CREATE TABLE stats (
+--     covid19 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     number_of_cases DECIMAL, 
+--     number_of_death DECIMAL,
+--     number_of_recoveries DECIMAL,
+--     PRIMARY KEY (covid19)
+-- );
 
-CREATE TABLE news (
-    covid19 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    local_news VARCHAR(100),
-    national_news VARCHAR(100),
-    global_news VARCHAR(100),
-    PRIMARY KEY (covid19)
-);
+-- CREATE TABLE news (
+--     covid19 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     local_news VARCHAR(100),
+--     national_news VARCHAR(100),
+--     global_news VARCHAR(100),
+--     PRIMARY KEY (covid19)
+-- );
 
-SELECT * FROM stats;
-SELECT * FROM news;
+-- SELECT * FROM stats;
+-- SELECT * FROM news;
 
-GET * FROM stats; https://covid-193.p.rapidapi.com/ 
-GET * FROM news; coronavirus-smartable.p.rapidapi.com
+-- GET * FROM stats; https://covid-193.p.rapidapi.com/ 
+-- GET * FROM news; coronavirus-smartable.p.rapidapi.com
--- database/seeds.sql
+++ database/seeds.sql
@@ -1,7 +1,1 @@
-INSERT INTO stats (number_of_cases) values ();
-INSERT INTO stats (number_of_death) values ();
-INSERT INTO stats (number_of_recoveries) values ();
-
-INSERT INTO news (local_news) values ();
-INSERT INTO news (national_news) values ();
-INSERT INTO news (global_news) values ();
+INSERT INTO Post (posts) values ();
