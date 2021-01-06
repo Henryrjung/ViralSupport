8841403a91f7b2d2d93fe88c79b26ca7421f8bda
HenryJ <henryjung667@gmail.com>
2021-01-05 22:15:52 -0600

    fixes

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,1 +1,3 @@
-INSERT INTO Post (posts) values ();
+INSERT INTO posts (id, title, body) values ("1", "Test", "This is a test post");
+INSERT INTO posts (id, title, body) values ("2", "Test", "This is a test post");
+INSERT INTO posts (id, title, body) values ("3", "Test", "This is a test post");
